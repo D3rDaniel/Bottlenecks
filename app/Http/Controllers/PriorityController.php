396ebc5c09d0c85f7695a8aab460d96964c37f5d
D3rDaniel <daniel.vogel1999@gmail.com>
2022-05-05 15:23:32 +0200

    TestCommit

---
+++ app/Http/Controllers/PriorityController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Priority;
+use Illuminate\Http\Request;
+
+class PriorityController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Priority  $priority
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Priority $priority)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Priority  $priority
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Priority $priority)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Priority  $priority
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Priority $priority)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Priority  $priority
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Priority $priority)
+    {
+        //
+    }
+}
